570bb53fc5273abb8f1930d717dd028373a12704
Anton Pegushin <anton.pegushin@gmail.com>
2023-06-25 19:47:25 -0700

    Initial commit of Notion-sdk package. First quick test.

--- cmd/notioncli/main.go
+++ cmd/notioncli/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/apegushin/notioncli/pkg/config"
+	"github.com/apegushin/notioncli/pkg/notion-sdk"
 )
 
 func main() {
@@ -15,6 +16,8 @@ func main() {
 	integrationToken := flag.String("integration-token", "", "Token supplied for integration by Notion My-Integrations")
 	integrationDatabaseId := flag.String("database-id", "", "ID of the database to which integration was applied")
 	flag.Parse()
+
+	// TODO: remove this and further extra logging
 	fmt.Printf("Defined and parsed CLI args\n")
 
 	if *integrationName != "" {
@@ -24,6 +27,9 @@ func main() {
 			os.Exit(1)
 		}
 	}
+
+	notion := notionsdk.NewClient("notioncli.cfg")
+	notion.Get()
 
 	fmt.Printf("Success! All done!\n")
 }
--- pkg/notion-sdk/notion-sdk.go
+++ pkg/notion-sdk/notion-sdk.go
@@ -1,3 +1,34 @@
 package notionsdk
 
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/apegushin/notioncli/pkg/config"
+)
+
+type Client struct {
+	cfg *config.Config
+}
+
+func NewClient(configFilePath string) *Client {
+	return &Client{
+		cfg: config.NewConfig(configFilePath),
+	}
+}
+
+func (c *Client) Get() error {
+	resp, err := http.Get("https://www.google.com/")
+
+	if err != nil {
+		fmt.Println("oh no, something terrible happened.", err)
+	} else {
+		if resp.Status == "200 OK" {
+			fmt.Println("got 200 OK from google.com")
+		} else {
+			fmt.Println("got some weird status", resp.Status)
+		}
+	}
+
+	return err
+}
